9e1bb6f57621c7b57c6c0b6b1154eef3ef974661
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2025-10-20 23:10:51 +0530

    Add Celsius to Fahrenheit conversion program

---
+++ Basic_programs/10_celcius_fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  float celcius,fahrenheit;
+  printf("enter the temperature in celcius");
+  scanf("%f",&celcius);
+  fahrenheit=(celcius*9/5)+32;
+  printf("the temperature in Fahrenheit is :%f",fahrenheit);
+
+}
