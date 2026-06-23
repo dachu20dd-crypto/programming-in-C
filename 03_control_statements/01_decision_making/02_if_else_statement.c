8ebf34d76c54bea1dfb2688e40057553d0786e65
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-06-23 20:39:04 +0530

    Add if-else statement for voting eligibility check

---
+++ 03_control_statements/01_decision_making/02_if_else_statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 16;
+
+    if(age >= 18)
+    {
+        printf("Eligible to vote");
+    }
+    else
+    {
+        printf("Not eligible to vote");
+    }
+
+    return 0;
+}
