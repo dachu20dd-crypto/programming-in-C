03ffad23131b721044e12a18d6f2e5cec28e1dc8
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-06-27 18:47:43 +0530

    Add string comparison example in compare_str.c

---
+++ 05_string_handling_functions/05_compare_str.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[] = "Apple";
+    char str2[] = "Apple";
+
+    if(strcmp(str1, str2) == 0)
+    {
+        printf("Strings are Equal");
+    }
+    else
+    {
+        printf("Strings are Not Equal");
+    }
+
+    return 0;
+}
