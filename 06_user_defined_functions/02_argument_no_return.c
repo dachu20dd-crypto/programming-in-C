0cd2599357906e02046732d836287019e3943acf
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-06-27 19:49:18 +0530

    Add function to calculate and print sum of two numbers

---
+++ 06_user_defined_functions/02_argument_no_return.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void add(int a, int b);
+
+int main()
+{
+    int x, y;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d", &x, &y);
+
+    add(x, y);
+
+    return 0;
+}
+
+void add(int a, int b)
+{
+    printf("Sum = %d", a + b);
+}
