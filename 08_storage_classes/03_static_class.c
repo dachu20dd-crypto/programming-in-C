bebf45537cba15dd9ee0ecfaf728a4d948018f91
DIVYADHARSHINI M <dachu20.dd@gmail.com>
2026-06-29 14:21:51 +0530

    Add static variable demonstration in static_class.c
    Implement static variable example in C.

---
+++ 08_storage_classes/03_static_class.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void count()
+{
+    static int num = 0;
+
+    num++;
+
+    printf("%d\n", num);
+}
+
+int main()
+{
+    count();
+    count();
+    count();
+
+    return 0;
+}
